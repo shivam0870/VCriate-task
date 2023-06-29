e1932e770793e30d5494ca43e61a29c66766e4b8
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-06-30 00:26:30 +0530

    Ecommerce done

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 4000.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Bed",
+    price: 199.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Television",
+    price: 700.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Car",
+    price: 23000000.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Bike",
+    price: 10000.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Jeans",
+    price: 100.0,
+    productImage: product6,
+  },
+];
